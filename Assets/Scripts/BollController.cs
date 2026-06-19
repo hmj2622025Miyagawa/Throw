641ea935155b6520f7985b607a01527cf536ba1b
hmj2622025Miyagawa <hmj2622025@stu.o-hara.ac.jp>
2026-06-19 16:46:57 +0900

    ボールが瓶に当たるようにした

--- Assets/Scripts/BollController.cs
+++ Assets/Scripts/BollController.cs
@@ -2,10 +2,20 @@ using UnityEngine;
 
 public class BollController : MonoBehaviour
 {
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce (dir);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        GetComponent<Rigidbody>().AddForce(dir);
+        Application.targetFrameRate = 60;
+        Shoot(new Vector3(0, 200, 2000));
     }
 
     // Update is called once per frame
